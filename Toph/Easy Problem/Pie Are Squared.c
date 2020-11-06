930447d4e1ca19ee718b24d3cf2f5c97d33b029d
ShahinRcpsc <71516989+ShahinRcpsc@users.noreply.github.com>
2020-11-06 19:33:58 +0600

    Create Pie Are Squared.c

---
+++ Toph/Easy Problem/Pie Are Squared.c
@@ -0,0 +1,15 @@
+/*
+Author: MSHS
+Created: 6 Nov, 2020
+Problem Link: https://toph.co/p/pie-are-squared
+
+*/
+
+
+#include <stdio.h>
+int main() {
+  float a, pi = 3.141592653589793;
+  scanf("%f", &a);
+  printf("%f\n", (a * a) * pi);
+  return 0;
+}
