cb112e5f5f015cc8d7573373570c0ad3fd442b70
ShahinRcpsc <71516989+ShahinRcpsc@users.noreply.github.com>
2020-11-06 19:27:07 +0600

    Create Add Them Up.c

---
+++ Toph/Easy Problem/Add Them Up.c
@@ -0,0 +1,15 @@
+/*
+Author: MSHS
+Created: 6 Nov, 2020
+Problem Link: https://toph.co/p/add-them-up
+
+*/
+
+
+#include <stdio.h>
+int main() {
+  int a, b;
+  scanf("%d%d", &a, &b);
+  printf("%d\n", a + b);
+  return 0;
+}
