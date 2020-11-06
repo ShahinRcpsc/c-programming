5eef145cae4e0bb33c7cb1429c37efeaf260bbae
ShahinRcpsc <71516989+ShahinRcpsc@users.noreply.github.com>
2020-11-06 19:25:22 +0600

    Create Copycat.c

---
+++ Toph/Easy Problem/Copycat.c
@@ -0,0 +1,15 @@
+/*
+Author: MSHS
+Created: 6 Nov, 2020
+Problem Link: https://toph.co/p/copycat
+
+*/
+
+
+#include <stdio.h>
+int main() {
+  int a;
+  scanf("%d", &a);
+  printf("%d\n", a);
+  return 0;
+}
