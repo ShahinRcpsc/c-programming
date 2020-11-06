674c25449a8541344c08bbc49dd2845387aea443
ShahinRcpsc <71516989+ShahinRcpsc@users.noreply.github.com>
2020-11-06 19:37:45 +0600

    Update Missing Number.c

--- Toph/Easy Problem/Missing Number.c
+++ Toph/Easy Problem/Missing Number.c
@@ -1,1 +1,16 @@
+/*
+Author: MSHS
+Created: 6 Nov, 2020
+Problem Link: https://toph.co/p/missing-number
 
+*/
+
+
+#include <stdio.h>
+int main() {
+  int a, b, c, d;
+  scanf("%d%d%d%d", &a, &b, &c, &d);
+  printf("%d", 10 - (b + c + d));
+
+  return 0;
+}
